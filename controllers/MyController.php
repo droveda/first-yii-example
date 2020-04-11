630c35b7427ade10c60c12dff74c83b4c8b83037
= <dieguesroveda@gmail.com>
2020-04-11 15:21:05 -0300

    routing rula example

---
+++ controllers/MyController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class MyController extends Controller {
+
+    public function actionMy() {
+        echo "My Action";
+    }
+
+}
+
+?>
